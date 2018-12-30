6bf2d90c7aded5be66ff1992d147ce814b808659
Steel Brain <i@steelbrain.me>
2018-12-31 00:32:26 +0500

    :bug: Fix typo (Steam -> Stream)

--- src/index.js
+++ src/index.js
@@ -7,19 +7,16 @@ import stripBomBuf from 'strip-bom-buf'
 const promisifiedFS = {}
 const syncMethods = ['Stats', '_toUnixTimestamp', 'watch', 'watchFile', 'unwatchFile']
 
-for (const key in fs) {
-  if (!{}.hasOwnProperty.call(fs, key)) {
-    continue
-  }
+Object.keys(fs).forEach(function(key) {
   const value = fs[key]
-  if (typeof value === 'function' && !key.includes('Sync') && !key.includes('Steam') && !syncMethods.includes(key)) {
+  if (typeof value === 'function' && !key.includes('Sync') && !key.includes('Stream') && !syncMethods.includes(key)) {
     promisifiedFS[key] = promisify(value)
   } else {
     promisifiedFS[key] = value
   }
-}
+})
 
-promisifiedFS.exists = function(path: string) {
+promisifiedFS.exists = function(path: string): Promise<boolean> {
   return new Promise(function(resolve) {
     fs.access(path, fs.R_OK, function(error) {
       resolve(error === null)
@@ -27,7 +24,7 @@ promisifiedFS.exists = function(path: string) {
   })
 }
 
-promisifiedFS.readFile = function(path: string, encoding: ?string) {
+promisifiedFS.readFile = function(path: string, encoding: ?string): Promise<string> {
   return new Promise(function(resolve, reject) {
     fs.readFile(path, function(error, buffer) {
       if (error) {
