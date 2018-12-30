d2aeb279d22b6ffd1e5fb3acb44afd89430ff21b
Steel Brain <i@steelbrain.me>
2018-12-31 01:39:43 +0500

    :new: Use ava for testing

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,51 +1,48 @@
-/* @flow */
-
-import Path from 'path'
-import { it } from 'jasmine-fix'
+import test from 'ava'
+import path from 'path'
 import vanilla from 'fs'
 import fs from '..'
 
-describe('sb-fs', function() {
-  it('exports consts properly', function() {
-    expect(typeof fs.R_OK).toBe('number')
-    expect(typeof fs.W_OK).toBe('number')
-  })
-  it('exports functions properly', function() {
-    expect(typeof fs.open).toBe('function')
-    expect(typeof fs.readFile).toBe('function')
-    expect(typeof fs.writeFile).toBe('function')
-  })
-  it('exports functions that work', async function() {
-    try {
-      await fs.access(__filename)
-    } catch (e) {
-      expect(false).toBe(true)
-    }
-    try {
-      await fs.access(Path.join(__dirname, 'test.bhrrr'))
-      expect(false).toBe(true)
-    } catch (e) {
-      /* No Op */
-    }
-  })
-  it('does not promisify sync functions', function() {
-    expect(fs.writeSync).toBe(vanilla.writeSync)
-    expect(fs.write).not.toBe(vanilla.write)
-    expect(fs.readSync).toBe(vanilla.readSync)
-    expect(fs.read).not.toBe(vanilla.read)
-    expect(fs.openSync).toBe(vanilla.openSync)
-    expect(fs.open).not.toBe(vanilla.open)
-  })
-  it('does not promisify non-callback functions', function() {
-    expect(fs.createReadStream).toBe(vanilla.createReadStream)
-    expect(fs.createWriteStream).toBe(vanilla.createWriteStream)
-    expect(fs.WriteStream).toBe(vanilla.WriteStream)
-    expect(fs.watch).toBe(vanilla.watch)
-    expect(fs.watchFile).toBe(vanilla.watchFile)
-    expect(fs.unwatchFile).toBe(vanilla.unwatchFile)
-  })
-  it('returns a BOM stripped string', async function() {
-    const contents = await fs.readFile(__filename, 'utf8')
-    expect(contents.charCodeAt(0)).not.toBe(0xfeff)
-  })
+test('exports consts properly', function(t) {
+  t.is(typeof fs.R_OK, 'number')
+  t.is(typeof fs.W_OK, 'number')
+})
+test('exports functions properly', function(t) {
+  t.is(typeof fs.open, 'function')
+  t.is(typeof fs.readFile, 'function')
+  t.is(typeof fs.writeFile, 'function')
+})
+test('exports functions that work', async function(t) {
+  try {
+    await fs.access(__filename)
+  } catch (e) {
+    t.fail()
+  }
+  try {
+    await fs.access(path.join(__dirname, 'test.bhrrr'))
+    t.fail()
+  } catch (e) {
+    /* No Op */
+  }
+  t.pass()
+})
+test('does not promisify sync functions', function(t) {
+  t.is(fs.writeSync, vanilla.writeSync)
+  t.not(fs.write, vanilla.write)
+  t.is(fs.readSync, vanilla.readSync)
+  t.not(fs.read, vanilla.read)
+  t.is(fs.openSync, vanilla.openSync)
+  t.not(fs.open, vanilla.open)
+})
+test('does not promisify non-callback functions', function(t) {
+  t.is(fs.createReadStream, vanilla.createReadStream)
+  t.is(fs.createWriteStream, vanilla.createWriteStream)
+  t.is(fs.WriteStream, vanilla.WriteStream)
+  t.is(fs.watch, vanilla.watch)
+  t.is(fs.watchFile, vanilla.watchFile)
+  t.is(fs.unwatchFile, vanilla.unwatchFile)
+})
+test('returns a BOM stripped string', async function(t) {
+  const contents = await fs.readFile(__filename, 'utf8')
+  t.not(contents.charCodeAt(0), 0xfeff)
 })
